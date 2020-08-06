e70b6a14b1ece7b7872236cc8dd047a01c87f4e6
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-08-05 20:57:15 -0500

    global components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ClientesComponent } from './pages/clientes/clientes.component';
 import { ProveedorComponent } from './pages/proveedor/proveedor.component';
 import { ProductosComponent } from './pages/productos/productos.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientesComponent,
     ProveedorComponent,
-    ProductosComponent
+    ProductosComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
